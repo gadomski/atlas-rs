650fd0528705ef1ff96cd4056eb26fafab3e959e
Pete Gadomski <pete.gadomski@gmail.com>
2016-07-22 10:50:53 -0600

    Add state of charge to the graphs

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ extern crate staticfile;
 
 use std::fmt::Write;
 
-use atlas::heartbeat::IntoHeartbeats;
+use atlas::heartbeat::{Heartbeat, IntoHeartbeats};
 
 use iron::prelude::*;
 use iron::headers::ContentType;
@@ -20,9 +20,12 @@ use router::Router;
 
 use staticfile::Static;
 
+const ATLAS_IMEI: &'static str = "300234063909200";
+
 fn main() {
     let mut router = Router::new();
     router.get("/", Static::new("html"));
+    router.get("/soc.csv", soc);
     router.get("/temperature.csv", temperature);
     let mut mount = Mount::new();
     mount.mount("/static/", Static::new("static"));
@@ -32,20 +35,29 @@ fn main() {
         .unwrap();
 }
 
-fn temperature(_: &mut Request) -> IronResult<Response> {
+fn heartbeats() -> Vec<Heartbeat> {
     let storage = sbd::storage::FilesystemStorage::open("/Users/gadomski/iridium").unwrap();
     let mut messages: Vec<_> = storage.iter().map(|r| r.unwrap()).collect();
-    messages.retain(|m| m.imei() == "300234063909200");
+    messages.retain(|m| m.imei() == ATLAS_IMEI);
     messages.sort();
-    let heartbeats = messages.into_heartbeats()
+    messages.into_heartbeats()
         .unwrap()
         .into_iter()
         .filter_map(|h| h.ok())
-        .collect::<Vec<_>>();
+        .collect::<Vec<_>>()
+}
+
+fn csv_response(csv: String) -> IronResult<Response> {
     let mut response = Response::new();
     response.status = Some(status::Ok);
     response.headers
         .set(ContentType(Mime(TopLevel::Text, SubLevel::Ext("csv".to_string()), vec![])));
+    response.body = Some(Box::new(csv));
+    Ok(response)
+}
+
+fn temperature(_: &mut Request) -> IronResult<Response> {
+    let heartbeats = heartbeats();
     let mut csv = String::new();
     writeln!(&mut csv, "Datetime,External,Mount").unwrap();
     for heartbeat in heartbeats {
@@ -56,6 +68,20 @@ fn temperature(_: &mut Request) -> IronResult<Response> {
                  heartbeat.temperature_mount,)
             .unwrap();
     }
-    response.body = Some(Box::new(csv));
-    Ok(response)
+    csv_response(csv)
+}
+
+fn soc(_: &mut Request) -> IronResult<Response> {
+    let heartbeats = heartbeats();
+    let mut csv = String::new();
+    writeln!(&mut csv, "Datetime,Battery #1,Battery #2").unwrap();
+    for heartbeat in heartbeats {
+        writeln!(&mut csv,
+                 "{},{:.2},{:.2}",
+                 heartbeat.messages.first().unwrap().time_of_session(),
+                 100.0 * heartbeat.soc1 / 5.0,
+                 100.0 * heartbeat.soc2 / 5.0)
+            .unwrap();
+    }
+    csv_response(csv)
 }
--- static/index.js
+++ static/index.js
@@ -1,18 +1,34 @@
-var temperature = new Dygraph(
-    document.getElementById("fig-temperature"),
-    "temperature.csv", {
-        rollPeriod: 6,
-        dateWindow: [Date.now() - 2 * 30 * 24 * 60 * 60 * 1000, Date.now()],
-        showRoller: true,
-        showRangeSelector: true,
-        labelsUTC: true,
-        height: 300,
-        axes: {
-          y: {
-            axisLabelFormatter: function(y) {
-              return y + '&deg;C';
-            }
-          }
+var options = {
+    rollPeriod: 6,
+    dateWindow: [Date.now() - 2 * 30 * 24 * 60 * 60 * 1000, Date.now()],
+    showRoller: true,
+    showRangeSelector: true,
+    labelsUTC: true,
+    height: 300
+};
+
+var socOptions = options;
+socOptions.axes = {
+    y: {
+        axisLabelFormatter: function(y) {
+            return y + '%';
         }
     }
-);
+};
+var soc = new Dygraph(
+    document.getElementById("fig-soc"),
+    "soc.csv", options);
+
+var temperatureOptions = options;
+temperatureOptions.axes = {
+    y: {
+        axisLabelFormatter: function(y) {
+            return y + '&deg;C';
+        }
+    }
+};
+var temperature = new Dygraph(
+    document.getElementById("fig-temperature"),
+    "temperature.csv", options);
+
+var sync = Dygraph.synchronize(soc, temperature, { range: false });
